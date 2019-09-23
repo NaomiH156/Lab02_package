418defcef5d26c5d9132f5f328778df9d19d3d85
Naomi <jdh156@case.edu>
2019-09-23 19:55:25 -0400

    update 7:55 PM. using rqt_gui to update des_vel crashes lab02_main_node and idk why

--- src/lab02_code.cpp
+++ src/lab02_code.cpp
@@ -13,24 +13,26 @@ float lin_vel = 0;
 float ang_vel = 0;
 std::vector<float> laser_data;
 
-//Put some callback functions here
-
-
 void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	laser_data = msg-> ranges;
+	//ROS_INFO("Laser data received!\n");
+	//This is working
 }
 
 void VelocityCallback(const geometry_msgs::Twist::ConstPtr& msg){
 	lin_vel = msg->linear.x;
 	ang_vel = msg->angular.z;
-	//printf("Desired velocities are:\n linear [%f]\n angular: [%f]\n", lin_vel, ang_vel);
-	ROS_INFO("Desired Velocities are: linear: [%f]\n angular: [%f]\n", lin_vel, ang_vel);
+	//ROS_INFO("Desired Velocities are: linear: [%f]\n angular: [%f]\n", lin_vel, ang_vel);
+	//This is working
 }
 
 int main(int argc, char **argv){
-	ROS_INFO("Main Node started!!\n");
+
 	ros::init(argc, argv, "lab02_package_node"); //does this have to match name of node in CMakeLists.txt?
 	ros::NodeHandle handle;
+
+	ROS_INFO("Main Node started!!\n");
+	//Why is the main function not getting called?
 //-------------------------------------------------------------------------------------------	
 	//"subscribe" notes:
 
@@ -41,11 +43,13 @@ int main(int argc, char **argv){
 	//second argument - size of message queue
 	//third argument - name of callback function to call
 	ros::Subscriber vel_sub = handle.subscribe("des_vel", 1, VelocityCallback);
+	//ROS_INFO ("Handle for des_vel Subscription set!\n");
 		//ros::Subscriber is like the variable type (like when you type "int x = 5;").
 		//"hand;e" is the node handle we created above
 		//subscribe() is a function (included in ros.h or something)
 		//"vel_sub" = "velocity subscription"
 	ros::Subscriber las_sub = handle.subscribe("/robot0/laser_1", 1, LaserCallback);
+	//ROS_INFO ("Handle for laser data subscription set!\n");
 		//"las_sub" = "laser subscription"
 		//MODIFY THIS CALL FOR IF THERE IS MORE THAN ONE ROBOT IN THE SIMULATION??
 //-------------------------------------------------------------------------------------------	
@@ -57,6 +61,7 @@ int main(int argc, char **argv){
 	//second argument to advertise is the name of the topic you are publishing to
 	//third argument is size of message queue
 	ros::Publisher vel_pub = handle.advertise<geometry_msgs::Twist>("robot0/cmd_vel", 1);
+	//ROS_INFO("Published to robot0/cmd_vel!\n");
 		//again, ros::Publisher is the variable type
 		//"handle" is the nodehandle from earlier
 		//advertise is a function. I guess you can have function<one_arg>(another_arg) in CPP?? idk
@@ -65,8 +70,8 @@ int main(int argc, char **argv){
 	//"spin" notes
 
 	//ros::spin() will enter a loop, pumping callbacks. Will exit w ctrl+C, or shutdown by master.
-	ros::spin();
-	ros::Rate loop_rate(10); //the loop time is every 10 ms?
+	//ros::spin(); DON'T USE THIS
+	ros::Rate loop_rate(10); //the loop rate is 10Hz
 
 //-----------------------------------------------------------------------------------------
 	//messages to be published
@@ -76,18 +81,24 @@ int main(int argc, char **argv){
 
 	while (ros::ok()){ //while the node is running?
 		geometry_msgs::Twist message;
+		//ROS_INFO("Message created!\n");
 			//creates a variable of type "geometry_msgs/Twist" called "message."
 		message.linear.x = lin_vel;
 		message.angular.z = ang_vel;
 			//"message" is formatted this way to conform with the type geometry_msgs/Twist
 			//recall that we included geometry_msgs/Twist in our header
-		if(lin_vel > 0 && laser_data [270] < 3){
+		if(lin_vel != 0 && laser_data [270] < 0.5){
+			//PUT MORE CODE HERE ABOUT HOW TO AVOID STUFF
 			message.linear.x = 0;
+			ROS_INFO("Robot stopped!\n");
 		}
 		vel_pub.publish(message);
+		//ROS_INFO("Published to robot0/cmd_vel!\n Desired velocity = [%f]\n", lin_vel);
 		ros::spinOnce();
 		loop_rate.sleep();
-		//go over the spinOnce and loop rate stuff
+		/* OKAY SO, WHEN YOU USE RQT_GUI to update des_vel, I get an error that says
+		[lab02_node_main] process has died. IDK why */
+
 	}
 	return 0;
 }//end of 'main' function
